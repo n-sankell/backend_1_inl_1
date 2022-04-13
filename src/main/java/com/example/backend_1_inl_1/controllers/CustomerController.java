4bd4b12842e91b344f900d794a79e921cc40fcf9
nicolasopazo <opazonico@gmail.com>
2022-04-13 12:15:25 +0200

    Added response class

--- src/main/java/com/example/backend_1_inl_1/controllers/CustomerController.java
+++ src/main/java/com/example/backend_1_inl_1/controllers/CustomerController.java
@@ -1,9 +1,11 @@
 package com.example.backend_1_inl_1.controllers;
 
 import com.example.backend_1_inl_1.models.Customer;
+import com.example.backend_1_inl_1.models.Response;
 import com.example.backend_1_inl_1.repositories.CustomerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -13,8 +15,13 @@ public class CustomerController {
     CustomerRepository customerRepository;
 
     @RequestMapping("/customers")
-    public Iterable<Customer> getAllCustomers() {
-        return customerRepository.findAll();
+    public Response<Iterable<Customer>> getAllCustomers() {
+        return new Response<>(customerRepository.findAll());
+    }
+
+    @RequestMapping("/customers/id")
+    public Customer getCustomerById(@RequestParam Long id) {
+        return customerRepository.findById(id).isPresent() ? customerRepository.findById(id).get() : new Customer();
     }
 
 }
---
+++ src/main/java/com/example/backend_1_inl_1/models/Response.java
@@ -0,0 +1,4 @@
+package com.example.backend_1_inl_1.models;
+
+public record Response<T>(T response) {
+}
