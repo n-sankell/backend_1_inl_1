8bc33e4c8e624f3397f55ecd2d16282a78f7f568
Niklas Sankell <niklas_san@gmail.com>
2022-04-14 00:47:12 +0200

    changed back to SQL

--- src/main/java/com/example/backend_1_inl_1/Backend1Inl1Application.java
+++ src/main/java/com/example/backend_1_inl_1/Backend1Inl1Application.java
@@ -9,6 +9,7 @@ import com.example.backend_1_inl_1.repositories.OrderRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 
 import java.time.LocalDate;
--- src/main/java/com/example/backend_1_inl_1/repositories/CustomerRepository.java
+++ src/main/java/com/example/backend_1_inl_1/repositories/CustomerRepository.java
@@ -2,6 +2,7 @@ package com.example.backend_1_inl_1.repositories;
 
 import com.example.backend_1_inl_1.models.Customer;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 public interface CustomerRepository extends CrudRepository<Customer, Long> {
 }
