47918b77215713a1d7f626438713ef710820c5ff
nicolasopazo <opazonico@gmail.com>
2022-04-13 15:19:10 +0200

    Order by customer id

--- src/main/java/com/example/backend_1_inl_1/controllers/OrderController.java
+++ src/main/java/com/example/backend_1_inl_1/controllers/OrderController.java
@@ -2,8 +2,10 @@ package com.example.backend_1_inl_1.controllers;
 
 import com.example.backend_1_inl_1.models.ItemOrder;
 import com.example.backend_1_inl_1.models.Response;
+import com.example.backend_1_inl_1.repositories.CustomerRepository;
 import com.example.backend_1_inl_1.repositories.OrderRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,10 +15,24 @@ public class OrderController {
 
     @Autowired
     OrderRepository orderRepository;
+    CustomerRepository customerRepository;
 
     @RequestMapping()
     public Response<Iterable<ItemOrder>> getAllOrders() {
         return new Response<>(orderRepository.findAll());
+    }
+
+    @RequestMapping("{id}")
+    public Response<?> getOrdersByCustomerId(@PathVariable String id) {
+        try {
+            long parsedId = Long.parseLong(id);
+            return customerRepository.findById(parsedId).isPresent()
+                    ? new Response<>(customerRepository.findById(parsedId).get().getItemOrders())
+                    : new Response<>("Customer not found.");
+        } catch (NumberFormatException e) {
+            e.printStackTrace();
+            return new Response<>("Please provide a valid number.");
+        }
     }
 
 }
